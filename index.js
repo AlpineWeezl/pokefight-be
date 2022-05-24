ec24e0388fa018cf02ba4ddf91cf84ec83198f06
Matthias Wieser <matthias.wieser.83@live.de>
2022-05-24 18:14:32 +0200

    add function to get data from JSON file

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { pokemonRouter } from './router/pokemon.js';
 
 const app = express();
 
+const port = process.env.PORT;
+
 // ###############################################
 // middlewares
 
@@ -14,3 +16,4 @@ app.use(express());
 
 app.use('/api/pokemon', pokemonRouter)
 
+app.listen(port, () => {console.log(`The server is listening on port ${port}`)})
