e6b482a06b2d64e873a2a350e1108c312095725a
Matthias Wieser <matthias.wieser.83@live.de>
2022-05-24 17:44:11 +0200

    insert json file fetch

--- controller/pokemon.js
+++ controller/pokemon.js
@@ -5,17 +5,19 @@
 // Read ---------------------------------------------------
 // All ---------------
 export const getAllPokemon = (req, res) => {
+    let jsonData = require('../pokedex.json');
 
 }
 
 // One by id or name -
 export const getOnePokemonByIdOrName = (req, res) => {
+    let jsonData = require('../pokedex.json');
 
 }
 
 // One Detail of one pokemon
 export const getOneDetailOfOnePokemon = (req, res) => {
-
+    let jsonData = require('../pokedex.json');
 }
 
 
